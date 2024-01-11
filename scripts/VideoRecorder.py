946fb61d69b8cdd72525a958110f08e1328f2e0e
Mohammad Hossein Bamorovat Abadi <m.bamorovvat@gmail.com>
2024-01-11 23:04:29 +0000

    edit the README.md and add param in launch file

--- scripts/VideoRecorder.py
+++ scripts/VideoRecorder.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+Author: Mohammad Hossein Bamorovat Abadi
+Date: 2024-01-11
+
+Description: This script subscribes to a list of ROS topics and records their images as videos.
+VideoRecorder class is the main class of this script. It takes a dictionary of topics and their corresponding folders
+as input. The dictionary should be in the following format:
+topics_to_folders = {
+    "/topic1": "folder1",
+    "/topic2": "folder2",
+    ... }
+The script creates a video writer for each topic and writes the images of that topic to the corresponding video file.
+The video files are saved in the output folder.
+
+Args:
+    topics_to_folders (dict): A dictionary of topics and their corresponding folders.
+    output_folder (str): The path to the output folder.
+    frame_rate (float): The frame rate of the videos.
+    debug (bool): If True, the script prints debugging information.
+    show_images (bool): If True, the script shows the images while recording.
+
+Usage:
+    roslaunch video_recorder video_recorder.launch
+    or
+    roslauch video_recorder video_recorder.launch bag_path:=/path/to/bag/file.bag output_folder:=/path/to/output/folder
+
+"""
+
 import os
 import rospy
 import cv2
@@ -11,9 +39,10 @@ import subprocess
 
 
 class VideoRecorder:
-    def __init__(self, topics_to_folders, output_folder='output', frame_rate=30.0, debug=True):
+    def __init__(self, topics_to_folders, output_folder='output', frame_rate=30.0, debug=True, show_images=False):
         # self.folder = None
         self.topics_to_folders = topics_to_folders
+        self.show_images = show_images
         self.bridge = CvBridge()
         self.frame_rate = frame_rate
         self.video_writers = {}
@@ -23,15 +52,11 @@ class VideoRecorder:
         self.frame_counters = {}  # Counter for each topic
         self.output_folder = output_folder
 
-        ros_pack = RosPack()
-        package_path = ros_pack.get_path('video_recorder')
-
-        view_path = os.path.join(package_path, self.output_folder)
-        if not os.path.exists(view_path):
-            os.makedirs(view_path)
+        if not os.path.exists(self.output_folder):
+            os.makedirs(self.output_folder)
 
         for topic, folder in self.topics_to_folders.items():
-            output_path = os.path.join(view_path, folder)
+            output_path = os.path.join(self.output_folder, folder)
             if not os.path.exists(output_path):
                 os.makedirs(output_path)
 
@@ -63,7 +88,8 @@ class VideoRecorder:
         if self.debug:
             print(f'Video file for topic {topic}: {video_file}')
 
-        self.video_writers[topic]['writer'] = cv2.VideoWriter(video_file, cv2.VideoWriter_fourcc(*'XVID'), self.frame_rate, frame_size)
+        self.video_writers[topic]['writer'] = cv2.VideoWriter(video_file, cv2.VideoWriter_fourcc(*'XVID'),
+                                                              self.frame_rate, frame_size)
 
         if self.debug:
             print(f'Initialized video writer for topic {topic} with frame size {frame_size}')
@@ -113,7 +139,7 @@ class VideoRecorder:
                     self.frame_counters[topic] += 1
                     print(f'Writing frame for topic {topic}')  # Debugging print
 
-            if self.debug:
+            if self.show_images:
                 cv2.imshow(self.folder[topic], frame)
                 cv2.waitKey(1)
 
@@ -136,15 +162,33 @@ class VideoRecorder:
                 writer_info['writer'].release()
 
 
-if __name__ == "__main__":
-    rospy.init_node('video_recorder', anonymous=True)
+def main():
+    rospy.init_node('video_recorder_node', anonymous=True)
 
     topics_to_folders = {
         "/head_camera/rgb/image_raw": "RobotView",
         "/rgbd_livingroom/rgb/image_raw": "FrontView",
-        # "/omni_livingroom/image_raw": "OmniView"
         # Add more topics and their corresponding folders here
     }
 
-    video_recorder = VideoRecorder(topics_to_folders, 'output1', debug=False)
-    video_recorder.play_rosbag('/media/abbas/Elements/Old_Hard/E/Ph.D. Abbas/My Dataset/action_recording_2.bag')
+    output_folder = rospy.get_param('~output_folder', '')  # Default is 'default_output' if not provided
+    rosbag_path = rospy.get_param('~rosbag_path', '')
+    debug = rospy.get_param('~debug', False)  # Default is False if not provided
+    show_images = rospy.get_param('~show_images', False)  # Default is False if not provided
+    frame_rate = rospy.get_param('~frame_rate', 20)  # Default is 30 if not provided
+
+    if debug:
+
+        print(f'output_folder: {output_folder}')
+        print(f'rosbag_path: {rosbag_path}')
+        print(f'debug: {debug}')
+        print(f'frame_rate: {frame_rate}')
+        print(f'show_images: {show_images}')
+
+    video_recorder = VideoRecorder(topics_to_folders, output_folder, frame_rate=frame_rate, debug=debug,
+                                   show_images=show_images)
+    video_recorder.play_rosbag(rosbag_path)
+
+
+if __name__ == "__main__":
+    main()
